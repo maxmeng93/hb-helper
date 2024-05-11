a01f42fb0257c5f4919b35d8feda0407425cc72a
maxmeng93 <mengyucheng@yogorobot.com>
2024-05-11 17:59:42 +0800

    feat: 修改样式&优化按钮禁用逻辑

--- background.js
+++ background.js
@@ -1,12 +1,12 @@
 // 监听安装事件
-// chrome.runtime.onInstalled.addListener(() => {
-//   console.log('扩展程序已安装');
+chrome.runtime.onInstalled.addListener(() => {
+  console.log("扩展程序已安装");
 
-//   // 插件上显示徽章
-//   chrome.action.setBadgeText({
-//     text: "ON",
-//   });
-// });
+  // 插件上显示徽章
+  // chrome.action.setBadgeText({
+  //   text: "ON",
+  // });
+});
 
 // 监听来自 content script 的消息
 // chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
--- popup/index.js
+++ popup/index.js
@@ -6,13 +6,38 @@
 
 const url = "https://m.touker.com/fd/conditions/monitoring";
 
-document.getElementById("hb").addEventListener("click", function () {
-  chrome.tabs.create({ url });
-});
+window.onload = () => {
+  setVersion();
+  addEvent();
+  checkIsHb();
+};
 
-document.getElementById('postpone').addEventListener('click', function () {
+function setVersion() {
+  var manifestData = chrome.runtime.getManifest();
+  var version = manifestData.version;
+  document.getElementById("version").textContent = `V${version}`;
+}
 
-  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-    chrome.tabs.sendMessage(tabs[0].id, { action: 'postpone' });
+function addEvent() {
+  document.getElementById("hb").addEventListener("click", function () {
+    chrome.tabs.create({ url });
   });
-});
+
+  document.getElementById("postpone").addEventListener("click", function () {
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+      chrome.tabs.sendMessage(tabs[0].id, { action: "postpone" });
+    });
+  });
+}
+
+function checkIsHb() {
+  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+    const postpone = document.getElementById("postpone");
+    var tab = tabs[0];
+    if (tab.url.startsWith(url)) {
+      postpone.disabled = false;
+    } else {
+      postpone.disabled = true;
+    }
+  });
+}
